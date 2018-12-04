2d42b9c59299ef616a8df68d67fe0fa4815345a6
djcopley <djrcopley@icloud.com>
2018-12-03 22:38:08 -0700

    Added base argument packages.

---
+++ src/argparse/arguments/PositionalArgument.java
@@ -0,0 +1,17 @@
+package argparse.arguments;
+
+public class PositionalArgument extends RequiredArgument {
+    private String input;
+
+    public PositionalArgument(String argument, String help) {
+        super(argument, help);
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
