9d3d8f40b36a874cd9d0c5227e20623344719e02
djcopley <djrcopley@icloud.com>
2018-12-16 18:33:55 -0700

    Updated tests in main

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,21 @@ public class Main {
         PositionalArgument arg2 = new PositionalArgument("foo", "Test argument");
         FlagArgument arg3 = new FlagArgument("--tesst", "Test argument");
         PositionalArgument arg4 = new PositionalArgument("doink", "Test argument");
-
+        PositionalArgument arg5 = new PositionalArgument("bar", "This argument is a positional argument");
 
         ArgumentParser parser = new ArgumentParser("Argument parser", args);
         parser.addArgument(arg1);
-        //parser.addArgument(arg2);
+        parser.addArgument(arg2);
         parser.addArgument(arg3);
-        //parser.addArgument(arg4);
+        parser.addArgument(arg4);
+        parser.addArgument(arg5);
 
         ArrayList<Argument> t = parser.parseArguments();
 
         System.out.println("Positional argument input:");
-        //System.out.println(arg2.getInput());
-        //System.out.println(arg4.getInput());
+        System.out.println(arg2.getInput());
+        System.out.println(arg4.getInput());
+        System.out.println(arg5.getInput());
 
         System.out.println("\nArguments passed:");
         for (Argument a : t) {
