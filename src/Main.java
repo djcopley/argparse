b02bae9ced0f8ff4affcc20a68267e4196319fdc
djcopley <djrcopley@icloud.com>
2018-12-17 23:22:04 -0700

    Updated tests.

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,27 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        FlagArgument arg1 = new FlagArgument("--test", "-t", "Test argument");
-        PositionalArgument arg2 = new PositionalArgument("foo", "Test argument");
-        FlagArgument arg3 = new FlagArgument("--tesst", "Test argument");
-        PositionalArgument arg4 = new PositionalArgument("doink", "Test argument");
-        PositionalArgument arg5 = new PositionalArgument("bar", "This argument is a positional argument");
+        PositionalArgument arg1 = new PositionalArgument("foo", "Test argument");
+        FlagInputArgument arg2 = new FlagInputArgument("--input", "-i", "Test argument");
+        FlagArgument arg3 = new FlagArgument("--verbose", "-v", "Test argument");
 
         ArgumentParser parser = new ArgumentParser("Argument parser", args);
+
         parser.addArgument(arg1);
         parser.addArgument(arg2);
         parser.addArgument(arg3);
-        parser.addArgument(arg4);
-        parser.addArgument(arg5);
 
         ArrayList<Argument> t = parser.parseArguments();
 
         System.out.println("Positional argument input:");
+        System.out.println(arg1.getInput());
+
+        System.out.println("\nFlag argument input:");
         System.out.println(arg2.getInput());
-        System.out.println(arg4.getInput());
-        System.out.println(arg5.getInput());
 
         System.out.println("\nArguments passed:");
         for (Argument a : t) {
-            if(a.isPassed()) {
+            if (a.isPassed()) {
                 System.out.println(a.getToken());
             }
         }
