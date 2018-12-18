40524fb1dc181f375085c5ad0170b1aba8702d7b
djcopley <djrcopley@icloud.com>
2018-12-17 21:43:58 -0700

    Refactored Argument and subclasses

--- src/argparse/arguments/OptionalArgument.java
+++ src/argparse/arguments/OptionalArgument.java
@@ -7,14 +7,91 @@ package argparse.arguments;
  * @version 0.1
  */
 public abstract class OptionalArgument extends Argument {
+    private String alias;
+
     /**
      * OptionalArgument class constructor. Constructor takes two string params and passes them to the super class
      * constructor.
      *
-     * @param argument Argument object added to parser
+     * @param token Argument object added to parser
      * @param help     text description of
      */
-    OptionalArgument(String argument, String help) {
-        super(argument, help);
+    OptionalArgument(String token, String help) {
+        super(token, help);
+        this.alias = "";
+    }
+
+    OptionalArgument(String token, String alias, String help) {
+        super(token, help);
+        this.alias = alias;
+    }
+
+    /**
+     * Method returns the arguments alias.
+     *
+     * @return alias
+     */
+    public String getAlias() {
+        return alias;
+    }
+
+    /**
+     * Method sets the alias field.
+     *
+     * @param alias value to assign "alias" field to
+     */
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    /**
+     * Method returns alias or token (if alias not present).
+     *
+     * @return command line token
+     */
+    @Override
+    public String getKeyword() {
+        if (alias.isEmpty()) {
+            return super.getKeyword();
+        }
+        return alias;
+    }
+
+    /**
+     * Method compares a string argument to it's token / alias and returns true if the match.
+     *
+     * @param arg string argument
+     * @return true if arg matches token / alias, else false
+     */
+    @Override
+    public boolean stringArgEquals(String arg) {
+        return arg.equals(getToken()) || arg.equals(getAlias());
+    }
+
+    /**
+     * Method compares two argument objects and returns true if their tokens/alias's match.
+     *
+     * @param other Argument object
+     * @return true if instance token/alias matches other token/alias, else false
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof FlagArgument)) {
+            return false;
+        }
+        return super.equals(other) || getAlias().equals(((FlagArgument) other).getAlias());
+    }
+
+    /**
+     * Method returns string representation of argument.
+     *
+     * @return string representation of argument
+     */
+    @Override
+    public String toString() {
+        if (!getAlias().isEmpty()) {
+            return String.format("%s, %s\t\t%s", getAlias(), getToken(), getHelp());
+        }
+        return super.toString();
     }
 }
--- src/argparse/arguments/RequiredArgument.java
+++ src/argparse/arguments/RequiredArgument.java
@@ -11,10 +11,10 @@ public abstract class RequiredArgument extends Argument {
      * RequiredArgument class constructor. Constructor takes two string params and passes them to the super class
      * constructor.
      *
-     * @param argument Argument object added to parser
+     * @param token Argument object added to parser
      * @param help     text to be shown to user when help flag is passed
      */
-    RequiredArgument(String argument, String help) {
-        super(argument, help);
+    RequiredArgument(String token, String help) {
+        super(token, help);
     }
 }
