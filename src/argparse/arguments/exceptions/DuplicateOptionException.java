f452620a59262e36051ff3ede958398ed4d99c0c
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2022-12-20 22:32:09 +0100

    Delete src/argparse/arguments/exceptions directory

--- src/argparse/arguments/exceptions/DuplicateOptionException.java
+++
@@ -1,9 +0,0 @@
-package argparse.arguments.exceptions;
-
-/**
- * DuplicateOptionException thrown when two arguments with the same token / alias are added to the parser.
- *
- * @author Daniel Copley
- * @version 0.1
- */
-public class DuplicateOptionException extends IllegalArgumentException {}
